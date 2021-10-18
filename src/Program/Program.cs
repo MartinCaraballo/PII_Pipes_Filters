77872c25d643fb9b8f645a0700bfd889d2c88188
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2021-10-18 16:13:12 -0300

    Exercise 1 solved

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -16,8 +16,15 @@ namespace CompAndDel
             IPipe pipeserial2 = new PipeSerial(filternegative, pipenull);
             IFilter filtergreyscale = new FilterGreyscale();
             IPipe pipeserial1 = new PipeSerial(filtergreyscale, pipeserial2);
+            
+            IPicture pictureFinal = pipeserial1.Send(picture);
+            IPicture pictureGrey = filtergreyscale.Filter(picture);
+            IPicture pictureNegative = filternegative.Filter(pictureGrey);
+            provider.SavePicture(pictureFinal, @"LukeFinal.jpg");
+            provider.SavePicture(pictureGrey, @"LukeGreyScale.jpg");
+            provider.SavePicture(pictureNegative, @"LukeNegative.jpg");
 
-            provider.SavePicture(picture, @"LukeFinal.jpg");
+            
 
         }
     }
