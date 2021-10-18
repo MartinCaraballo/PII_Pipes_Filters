79dde7b1322b8cf1ade13d489dfa5873f971d549
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2021-10-18 10:55:25 -0300

    Challenge 1 solved

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,17 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"Luke.jpg");
+
+            IPipe pipenull = new PipeNull();
+            IFilter filternegative = new FilterNegative();
+            IPipe pipeserial2 = new PipeSerial(filternegative, pipenull);
+            IFilter filtergreyscale = new FilterGreyscale();
+            IPipe pipeserial1 = new PipeSerial(filtergreyscale, pipeserial2);
+
+            provider.SavePicture(picture, @"LukeFinal.jpg");
+
         }
     }
 }
